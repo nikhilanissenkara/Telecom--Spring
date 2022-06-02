641cddeaa752f2759a52753deb462066429126ea
nikhilanissenkara <nvsnikhila0@gmail.com>
2022-06-02 10:31:22 +0530

    just adding spaces

--- src/main/java/org/telecom/telecom_repository/TelecomRepository.java
+++ src/main/java/org/telecom/telecom_repository/TelecomRepository.java
@@ -6,4 +6,6 @@ import org.telecom.telecom_Entity.SimDetails;
 public interface TelecomRepository extends CrudRepository<SimDetails,Integer>
 {
       SimDetails FindBySimNumberAndServiceNumber(Long simNumber,Long serviceNumber);
+
+
 }
